dcf3ffb792e69cdac8d5553d8c45296e025d19ad
Nenkai <Nenkai@users.noreply.github.com>
2024-09-13 19:10:35 +0200

    Bump version

--- FF16Tools.CLI/Program.cs
+++ FF16Tools.CLI/Program.cs
@@ -22,7 +22,7 @@ namespace FF16Tools.CLI;
 
 public class Program
 {
-    public const string Version = "1.2.2";
+    public const string Version = "1.3.0";
 
     private static ILoggerFactory _loggerFactory;
     private static Microsoft.Extensions.Logging.ILogger _logger;
