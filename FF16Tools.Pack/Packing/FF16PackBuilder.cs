027fcfbf426f6dc54f5a83fe515ccb7d6cc0bce6
Nenkai <Nenkai@users.noreply.github.com>
2024-09-18 05:13:03 +0200

    further normalize

--- FF16Tools.Pack/Packing/FF16PackBuilder.cs
+++ FF16Tools.Pack/Packing/FF16PackBuilder.cs
@@ -128,7 +128,7 @@ public class FF16PackBuilder
         if (!gamePath.StartsWith(_options.Name))
             throw new ArgumentException($"Game path should start with '{_options.Name}'.");
 
-        gamePath = Path.GetRelativePath(_options.Name, gamePath);
+        gamePath = Path.GetRelativePath(_options.Name, gamePath).Replace('\\', '/'); // normalize again since GetRelativePath can swap back to '\\'..
 
         _logger?.LogInformation("PACK: Adding '{path}'...", gamePath);
 
