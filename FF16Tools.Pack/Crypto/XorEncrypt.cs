82f01ef240412caf49da780580b05bcf231e46bc
Nenkai <Nenkai@users.noreply.github.com>
2025-01-23 15:12:29 +0100

    hotfix: namespace

--- FF16Tools.Pack/Crypto/XorEncrypt.cs
+++ FF16Tools.Pack/Crypto/XorEncrypt.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FF16Tools.Crypto;
+namespace FF16Tools.Pack.Crypto;
 
 public class XorEncrypt
 {
