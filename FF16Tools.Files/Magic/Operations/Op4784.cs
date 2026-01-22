9eaaaa846f7fc90373824a9e0244c39504f500e4
mThund3R <90071076+mThund3R@users.noreply.github.com>
2026-01-22 21:40:57 +0100

    Update Op4784.cs
    These changes solve loading failures within the magic editor happening with files containing previously unsupported property types on Op4784.

--- FF16Tools.Files/Magic/Operations/Op4784.cs
+++ FF16Tools.Files/Magic/Operations/Op4784.cs
@@ -11,6 +11,35 @@ public class Op4784 : MagicOperationBase<Op4784>, IOperationBase<Op4784>
     public override MagicOperationType Type => MagicOperationType.Operation_4784;
     public static HashSet<MagicPropertyType> sSupportedProperties { get; set; } =
     [
-        MagicPropertyType.ProjectileHitboxRadiusStart
+        MagicPropertyType.ProjectileHitboxRadiusStart,
+        MagicPropertyType.Prop_105,
+        MagicPropertyType.ActorEidForProjectileSource,
+        MagicPropertyType.ActorVFXAudioId,
+        MagicPropertyType.OnFinishedOpGroupId,
+        MagicPropertyType.Prop_104,
+        MagicPropertyType.Prop_188,
+        MagicPropertyType.Prop_995,
+        MagicPropertyType.Projectile1XYZOffset,
+        MagicPropertyType.Projectile2XYZOffset,
+        MagicPropertyType.Prop_24,
+        MagicPropertyType.ProjectileSpeedStart,
+        MagicPropertyType.ProjectileHitboxRadiusIncreaseRate,
+        MagicPropertyType.NumProjectilesToSpawn,
+        MagicPropertyType.Prop_4277,
+        MagicPropertyType.ProjectileHitboxRadiusStart,
+        MagicPropertyType.ProjectileSpeedIncreaseRate,
+        MagicPropertyType.Prop_148,
+        MagicPropertyType.ProjectileOnHitAttackParamId,
+        MagicPropertyType.Prop_2060,
+        MagicPropertyType.Prop_109,
+        MagicPropertyType.Prop_30,
+        MagicPropertyType.Prop_3856,
+        MagicPropertyType.Prop_3848,
+        MagicPropertyType.Prop_4846,
+        MagicPropertyType.Prop_28,
+        MagicPropertyType.Prop_3722_EidId,
+        MagicPropertyType.Prop_995,
+        MagicPropertyType.Prop_4277,
+        MagicPropertyType.Prop_78
     ];
 }
