0f3a3255a5404085c998b9d8debc2f3c1d10f383
Nenkai <Nenkai@users.noreply.github.com>
2026-02-01 18:08:14 +0100

    CLI: bump header version

--- FF16Tools.CLI/Program.cs
+++ FF16Tools.CLI/Program.cs
@@ -23,7 +23,7 @@ namespace FF16Tools.CLI;
 
 public class Program
 {
-    public const string Version = "1.12.0";
+    public const string Version = "1.13.0";
 
     private static ILoggerFactory _loggerFactory;
     private static Microsoft.Extensions.Logging.ILogger _logger;
